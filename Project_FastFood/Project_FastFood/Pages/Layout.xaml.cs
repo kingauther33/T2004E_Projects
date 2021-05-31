3a95b7d0c9bfc7a7d95ee7d1b5fb0d97e5f959fa
kingauther33 <64845909+kingauther33@users.noreply.github.com>
2021-05-31 20:20:18 +0700

    Commit

---
+++ Project_FastFood/Project_FastFood/Pages/Layout.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Project_FastFood.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Layout : Page
+    {
+        public Layout()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HomeNavigate(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(Pages.Home));
+        }
+    }
+}
