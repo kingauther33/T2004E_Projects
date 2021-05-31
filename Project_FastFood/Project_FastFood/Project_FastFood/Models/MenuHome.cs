4ed8ca643f3fa1938f40cb2f41b8bee085cc9146
kingauther33 <64845909+kingauther33@users.noreply.github.com>
2021-05-31 21:24:24 +0700

    Stash Bin and Obj

---
+++ Project_FastFood/Project_FastFood/Project_FastFood/Models/MenuHome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_FastFood.Models
+{
+    class MenuHome
+    {
+        public class Datum
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string image { get; set; }
+            public string description { get; set; }
+            public int price { get; set; }
+        }
+
+        public class Root
+        {
+            public string message { get; set; }
+            public List<Datum> data { get; set; }
+        }
+    }
+}
