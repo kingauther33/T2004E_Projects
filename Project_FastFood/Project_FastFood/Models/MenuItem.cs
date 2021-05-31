4a30ac6a59b310feed6d3a5954d96a7878acd643
kingauther33 <64845909+kingauther33@users.noreply.github.com>
2021-05-31 20:26:25 +0700

    Commit

---
+++ Project_FastFood/Project_FastFood/Models/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_FastFood.Models
+{
+    class MenuItem
+    {
+        public string Icon { get; set; }
+
+        public string Name { get; set; }
+
+        public string MenuPage { get; set; }
+
+        public MenuItem(string icon, string name, string menuPage)
+        {
+            Icon = icon;
+            Name = name;
+            MenuPage = menuPage;
+        }
+    }
+}
--- Project_FastFood/Project_FastFood/Pages/Layout.xaml.cs
+++ Project_FastFood/Project_FastFood/Pages/Layout.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+<<<<<<< HEAD
+=======
+using Project_FastFood.Models;
+>>>>>>> parent of ae6be87 (Update)
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -13,11 +17,47 @@ namespace Project_FastFood.Pages
         public Layout()
         {
             this.InitializeComponent();
+<<<<<<< HEAD
         }
 
         private void HomeNavigate(object sender, RoutedEventArgs e)
         {
             MainFrame.Navigate(typeof(Pages.Home));
+=======
+            MainFrame.Navigate(typeof(Pages.Home));
+        }
+
+        //private void HomeNavigate(object sender, RoutedEventArgs e)
+        //{
+        //    MainFrame.Navigate(typeof(Pages.Home));
+        //}
+
+        private void menuList_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Add item to loaded
+            menuList.Items.Add(new MenuItem("\uE770", "Home", "home"));
+            menuList.Items.Add(new MenuItem("\uE80F", "Eat-in", "eat-in"));
+            menuList.Items.Add(new MenuItem("\uE8FD", "Collection", "collection"));
+            menuList.Items.Add(new MenuItem("\uE7EC", "Delivery", "delivery"));
+            menuList.Items.Add(new MenuItem("\uED56", "Take Away", "take away"));
+            menuList.Items.Add(new MenuItem("\uE8C7", "Driver Payment", "driver payment"));
+            menuList.Items.Add(new MenuItem("\uE716", "Customers", "customers"));
+        }
+
+        private void menuList_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            MenuItem selectedItem = menuList.SelectedItem as MenuItem; // MenuItem Clicked
+            switch (selectedItem.MenuPage)
+            {
+                case "home": MainFrame.Navigate(typeof(Pages.Home)); break;
+                case "eat-in": MainFrame.Navigate(typeof(Pages.Eat_In)); break;
+                case "collection": MainFrame.Navigate(typeof(Pages.Collection), "This is Collection Page"); break;
+                case "delivery": MainFrame.Navigate(typeof(Pages.Delivery), "This is Delivery Page"); break;
+                case "take away": MainFrame.Navigate(typeof(Pages.Take_Away), "This is Take Away Page"); break; 
+                case "driver payment": MainFrame.Navigate(typeof(Pages.Driver_Payment), "This is Driver Payment Page"); break; 
+                case "customers": MainFrame.Navigate(typeof(Pages.Customer), "This is Customer Page"); break; 
+            }
+>>>>>>> parent of ae6be87 (Update)
         }
     }
 }
